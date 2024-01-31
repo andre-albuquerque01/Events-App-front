49e1de7e53d0f7fbb894d288b71fc15a0e2559e1
Andre <andrealbuquerque2001@gmail.com>
2024-01-31 18:05:28 -0300

    route

--- src/app/api/events/hasEvent/[page]/route.ts
+++ src/app/api/events/hasEvent/[page]/route.ts
@@ -1,36 +1,29 @@
 import { cookies } from 'next/headers'
 
 export async function GET(
-  _: Request,
-  { params }: { params: { page: number } },
+  request: Request,
+  { params }: { params: { page: number; token: string } },
 ) {
   try {
     const cookiesStore = cookies()
     const token = cookiesStore.get('token')
+    console.log(token?.value)
 
     const response = await fetch(
       `http://localhost/api/hasEvents?page=${params.page}`,
       {
         method: 'GET',
-        cache: 'no-cache',
         headers: {
-          'Content-Type': 'application/json',
           Authorization: `Bearer ${token?.value}`,
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
         },
       },
     )
-    if (response.headers.get('content-type') === 'application/json') {
-      const data = await response.json()
-      console.log(data)
-    } else {
-      console.error('Received non-JSON response:', await response.text())
-    }
 
-    if (!response) {
-      return Response.json({ message: 'Not found.' }, { status: 400 })
-    }
-
-    // return Response.json({ data })
+    const data = await response.json()
+    console.log(data)
+    return Response.json({ data })
   } catch (error) {
     console.error(error)
   }
